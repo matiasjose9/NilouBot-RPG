c010c89217ff9e46d47a2538f9f0ba182bec2391
matias-crypto <149725150+matias-crypto@users.noreply.github.com>
2024-07-09 01:38:20 -0300

    Actualizar ingles.js

--- idiomas/ingles.js
+++ idiomas/ingles.js
@@ -1,8 +1,12 @@
 export default {
-  commands: {
-    fb: {
-      enterLink: 'Enter a Facebook link',
-      title: 'Title'
-    }
+  ejemplo: {
+    mensajeInicial: 'Hello! I am an example of a multilingual command.',
+    mensajeCambio: 'The language has been changed to English.'
+  },
+  language: {
+    changedToSpanish: 'El idioma ha sido cambiado a español.',
+    changedToEnglish: 'El idioma ha sido cambiado a inglés.',
+    specifyLanguage: 'Please specify the language (Spanish or English).',
+    unrecognizedLanguage: 'Unrecognized language. Please use "Spanish" or "English".'
   }
 };
