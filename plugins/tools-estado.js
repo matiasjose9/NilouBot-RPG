b6b30003ce04cc2e59739bfe3ee87c6b292e6e03
matias-crypto <149725150+matias-crypto@users.noreply.github.com>
2024-07-09 01:14:57 -0300

    Actualizar tools-estado.js

--- plugins/tools-estado.js
+++ plugins/tools-estado.js
@@ -1,66 +1,90 @@
-
 import { performance } from 'perf_hooks'
 import osu from 'node-os-utils'
 
-var handler = async(m, { conn, command, usedPrefix}) => {
+const handler = async (m, { conn, command }) => {
+  try {
+    const NotDetect = 'No Detectado'
+    const startTime = performance.now()
 
-try {
+    const cpu = osu.cpu
+    const drive = osu.drive
+    const mem = osu.mem
+    const netstat = osu.netstat
+    const osPlatform = osu.os.platform()
 
-let NotDetect = 'No Detectado'
-let old = performance.now()
-let cpu = osu.cpu
-let cpuCore = cpu.count()
-let drive = osu.drive
-let mem = osu.mem
-let netstat = osu.netstat
-let OS = osu.os.platform()
-let cpuModel = cpu.model()
-let cpuPer
-let p1 = cpu.usage().then(cpuPercentage => { cpuPer = cpuPercentage }).catch(() => { cpuPer = NotDetect })
-let driveTotal, driveUsed, drivePer
-let p2 = drive.info().then(info => { driveTotal = (info.totalGb + ' GB'), driveUsed = info.usedGb, drivePer = (info.usedPercentage + '%') }).catch(() => { driveTotal = NotDetect, driveUsed = NotDetect, drivePer = NotDetect })
-let ramTotal, ramUsed
-let p3 = mem.info().then(info => { ramTotal = info.totalMemMb, ramUsed = info.usedMemMb }).catch(() => { ramTotal = NotDetect, ramUsed = NotDetect })
-let netsIn, netsOut
-let p4 = netstat.inOut().then(info => { netsIn = (info.total.inputMb + ' MB'), netsOut = (info.total.outputMb + ' MB') }).catch(() => { netsIn = NotDetect, netsOut = NotDetect })
-await Promise.all([p1, p2, p3, p4])
-await conn.reply(m.chat, `_Probando ${command }..._`, m)
-let _ramTotal = (ramTotal + ' MB')
-let neww = performance.now()
+    const cpuInfo = {
+      model: await cpu.model().catch(() => NotDetect),
+      cores: await cpu.count().catch(() => NotDetect),
+      usage: await cpu.usage().catch(() => NotDetect)
+    }
 
+    const driveInfo = await drive.info().catch(() => ({
+      totalGb: NotDetect,
+      usedGb: NotDetect,
+      usedPercentage: NotDetect
+    }))
 
-var txt = `🚩 *Estado*
+    const memInfo = await mem.info().catch(() => ({
+      totalMemMb: NotDetect,
+      usedMemMb: NotDetect
+    }))
 
-*OS* : ${OS}
-*CPU Model* : ${cpuModel}
-*CPU Core* : ${cpuCore} Centro
-*CPU* : ${cpuPer}%
-*Ram* : ${ramUsed} / ${_ramTotal}(${/[0-9.+/]/g.test(ramUsed) &&  /[0-9.+/]/g.test(ramTotal) ? Math.round(100 * (ramUsed / ramTotal)) + '%' : NotDetect})
-*Drive* : ${driveUsed} / ${driveTotal} (${drivePer})
-*Ping* : ${Math.round(neww - old)} ms
-*Internet IN* : ${netsIn}
-*Internet OUT* : ${netsOut}`
+    const netstatInfo = await netstat.inOut().catch(() => ({
+      total: { inputMb: NotDetect, outputMb: NotDetect }
+    }))
 
-conn.relayMessage(m.chat, { extendedTextMessage:{ text: txt, contextInfo: { externalAdReply: { title: '', mediaType: 1, previewType: 0, renderLargerThumbnail: true, thumbnailUrl: 'https://telegra.ph/file/56d6e15d7ebf5fc5d0d26.jpg', sourceUrl: '' }}, mentions: [m.sender] }}, {})
+    await conn.reply(m.chat, `_Probando ${command}..._`, m)
 
-console.log(OS)
-} catch (e) {
-console.log(e)
-conn.reply(m.chat, '🚩 *Ocurrió un fallo*', m, fake, )}
+    const ramTotal = `${memInfo.totalMemMb} MB`
+    const ramUsed = `${memInfo.usedMemMb} MB`
+    const ramUsage = (/[0-9.+/]/g.test(memInfo.usedMemMb) && /[0-9.+/]/g.test(memInfo.totalMemMb))
+      ? `${Math.round((memInfo.usedMemMb / memInfo.totalMemMb) * 100)}%`
+      : NotDetect
 
+    const endTime = performance.now()
+    const ping = `${Math.round(endTime - startTime)} ms`
+
+    const message = `
+🚩 *Estado*
+
+*OS*: ${osPlatform}
+*CPU Model*: ${cpuInfo.model}
+*CPU Cores*: ${cpuInfo.cores}
+*CPU Usage*: ${cpuInfo.usage}%
+*RAM Usage*: ${ramUsed} / ${ramTotal} (${ramUsage})
+*Drive Usage*: ${driveInfo.usedGb} / ${driveInfo.totalGb} (${driveInfo.usedPercentage}%)
+*Ping*: ${ping}
+*Internet IN*: ${netstatInfo.total.inputMb} MB
+*Internet OUT*: ${netstatInfo.total.outputMb} MB
+    `
+
+    conn.relayMessage(m.chat, {
+      extendedTextMessage: {
+        text: message.trim(),
+        contextInfo: {
+          externalAdReply: {
+            title: '',
+            mediaType: 1,
+            previewType: 0,
+            renderLargerThumbnail: true,
+            thumbnailUrl: 'https://telegra.ph/file/56d6e15d7ebf5fc5d0d26.jpg',
+            sourceUrl: ''
+          }
+        },
+        mentions: [m.sender]
+      }
+    }, {})
+
+    console.log(osPlatform)
+  } catch (e) {
+    console.log(e)
+    conn.reply(m.chat, '🚩 *Ocurrió un fallo*', m)
+  }
 }
-handler.help = ['estado', 'status']
-handler.tags = ['bot']
-handler.command = /^status|statusbot|botstatus|estado$/i
 
+handler.help = ['estado', 'status']
+handler.tags = ['tools']
+handler.command = /^status|statusbot|botstatus|estado$/i
 handler.register = true
 
 export default handler
-
-function clockString(ms) {
-let h = Math.floor(ms / 3600000)
-let m = Math.floor(ms / 60000) % 60
-let s = Math.floor(ms / 1000) % 60
-console.log({ ms, h, m, s })
-return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
-}
