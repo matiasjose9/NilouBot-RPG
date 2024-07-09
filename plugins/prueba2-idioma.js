e50a04f1e54458497e6a855588b05b0c93e9e5f1
matias-crypto <149725150+matias-crypto@users.noreply.github.com>
2024-07-09 03:33:09 -0300

    Actualizar prueba2-idioma.js

--- plugins/prueba2-idioma.js
+++ plugins/prueba2-idioma.js
@@ -1,24 +1,23 @@
-import fs from 'fs'
+import fs from 'fs';
 
 const handler = async (m) => {
     try {
-        const data = global
-        const idioma = data.db.data.users[m.sender].language
-        const _translate = JSON.parse(fs.readFileSync(`./idiomas/${idioma}.json`))
-        const tradutor = _translate.plugins._general
+        const data = global;
+        const idioma = data.db.data.users[m.sender].language;
+        const _translate = JSON.parse(fs.readFileSync(`./idiomas/${idioma}.json`, 'utf8'));
+        const traductor = _translate.plugins._general;
 
-const info = `
-{traductor.texto1}
+        const info = `
+${traductor.texto1.join('\n\n')}
+${traductor.texto2.join('\n\n')}
+`;
 
-{traductor.texto2}
-`
-
-        m.reply(info)
+        m.reply(info);
     } catch (error) {
-        m.reply(`*[ERROR]* - _Error al cargar el idioma._`)
+        m.reply(`*[ERROR]* - _Error al cargar el idioma._`);
     }
-}
+};
 
-handler.command = /^(sal)$/i
+handler.command = /^(sal)$/i;
 
-export default handler
+export default handler;
