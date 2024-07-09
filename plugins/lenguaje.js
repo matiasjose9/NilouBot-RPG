0bc87e4510db7c33b32b4ff30e56a6dadd5af319
matias-crypto <149725150+matias-crypto@users.noreply.github.com>
2024-07-09 02:58:47 -0300

    Actualizar lenguaje.js

--- plugins/lenguaje.js
+++ plugins/lenguaje.js
@@ -1,39 +1,77 @@
-import { promises as fs } from 'fs';
+import fs from 'fs'
 
 const handler = async (m, { args, usedPrefix, command, isAdmin }) => {
   try {
-    const data = global;
-    let idioma = data.db.data.users[m.sender].language || 'es';
-
+    const data = global
+    let idioma = data.db.data.users[m.sender].language || 'es' // Idioma predeterminado
+    
+    // Verificar si el archivo de idioma existe
     if (!fs.existsSync(`./idiomas/${idioma}.json`)) {
-      throw new Error(`Archivo de idioma no encontrado: ./idiomas/${idioma}.json`);
+      throw new Error(`Archivo de idioma no encontrado: ./idiomas/${idioma}.json`)
     }
 
-    const _translate = JSON.parse(await fs.readFile(`./idiomas/${idioma}.json`));
-    const tradutor = _translate.plugins._language;
+    const _translate = JSON.parse(fs.readFileSync(`./idiomas/${idioma}.json`))
+    const tradutor = _translate.plugins._language
 
-    let sigla = args[0] ? args[0].toLowerCase() : '';
+    let sigla
+
+    if (args[0] != undefined) {
+      sigla = args[0].toLowerCase()
+    }
 
     if (command === 'lang') {
-      if (sigla === 'es' || sigla === 'en' || sigla === 'ar') {
-        global.db.data.users[m.sender].language = sigla;
-        let languageName = sigla === 'es' ? 'Español 🇪🇸' :
-                           sigla === 'en' ? 'English 🇺🇸' :
-                           sigla === 'ar' ? 'العربية 🇸🇦' : '';
-        m.reply(`*Nilou - Bot*\n\n*—◉* *_Idioma definido a ${languageName}_*`);
+      if (sigla === 'es') {
+        global.db.data.users[m.sender].language = 'es'
+        m.reply(`*[ ✅ ] Nilou - Bot*\n\n*—◉* *_Idioma definido a Español 🇪🇸_*`)
+      } else if (sigla === 'en') {
+        global.db.data.users[m.sender].language = 'en'
+        m.reply(`*[ ✅ ] Nilou - Bot*\n\n*—◉* *_Idioma definido a Inglés 🇬🇧_*`)
       } else {
-        throw new Error(`${tradutor.texto1[2]}\n${tradutor.texto1[3]} (${data.db.data.users[m.sender].language})\n${tradutor.texto1[0]}\n*${usedPrefix}lang* es\n\n${tradutor.texto1[1]}`);
+        m.reply(`
+${tradutor.texto1[2]}
+${tradutor.texto1[3]} *( ${data.db.data.users[m.sender].language} )*
+${tradutor.texto1[0]}
+*${usedPrefix}lang* es
+
+${tradutor.texto1[1]}
+`)
       }
     }
 
+    if (command === 'langgroup') {
+      if (m.isGroup === false) {
+        return m.reply(tradutor.texto3)
+      }
+      if (m.isGroup === true && isAdmin === false) {
+        return m.reply(tradutor.texto4)
+      }
+
+      if (sigla === 'es') {
+        global.db.data.chats[m.chat].language = 'es'
+        m.reply(`*[ ✅ ] Configuración del grupo*\n\n*—◉* *_Idioma definido a Español 🇪🇸_*`)
+      } else if (sigla === 'en') {
+        global.db.data.chats[m.chat].language = 'en'
+        m.reply(`*[ ✅ ] Configuración del grupo*\n\n*—◉* *_Idioma definido a Inglés 🇬🇧_*`)
+      } else {
+        m.reply(`
+${tradutor.texto2[0]}
+*${usedPrefix}langgroup* es
+
+${tradutor.texto2[1]}
+`)
+      }
+    }
   } catch (error) {
-    console.error(error);
-    global.db.data.users[m.sender].language = 'es';
-    m.reply(`*[ERROR]* - _Por defecto el idioma estaba configurado en español._
-\`\`\`Contacta a los creadores del bot\`\`\` `);
+    console.error(error) // Mensaje de depuración
+    global.db.data.users[m.sender].language = 'es'
+    if (m.isGroup) {
+      global.db.data.chats[m.chat].language = 'es'
+    }
+    m.reply(`*[ERROR]* -  _Por defecto el idioma estaba configurado en español._
+\`\`\`contacta a los creadores del bot\`\`\` `)
   }
-};
+}
 
-handler.command = /^(lang)$/i;
+handler.command = /^(lang|langgroup)$/i
 
-export default handler;
+export default handler
