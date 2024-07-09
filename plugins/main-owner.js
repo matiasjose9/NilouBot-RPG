16daa7a24af1ea9b62a9c0b03afe6c2dc908bef2
matias-crypto <149725150+matias-crypto@users.noreply.github.com>
2024-07-09 03:52:43 -0300

    Actualizar main-owner.js

--- plugins/main-owner.js
+++ plugins/main-owner.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, usedPrefix, isOwner }) => {
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;Enzo;;\nFN:JTxꜱ\nORG:JTxꜱ\nTITLE:\nitem1.TEL;waid=5491126788746:5491126788746\nitem1.X-ABLabel:JTxꜱ\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:JTxꜱ\nEND:VCARD`
-await conn.sendMessage(m.chat, { contacts: { displayName: 'JTxꜱ', contacts: [{ vcard }] }}, {quoted: m})
+let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;Enzo;;\nFN:Enzo\nORG:JTxꜱ\nTITLE:\nitem1.TEL;waid=5491126788746:5491126788746\nitem1.X-ABLabelEnzo\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:Enzo\nEND:VCARD`
+await conn.sendMessage(m.chat, { contacts: { displayName: 'Enzo', contacts: [{ vcard }] }}, {quoted: m})
 }
 handler.help = ['owner']
 handler.tags = ['main']
