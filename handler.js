701558c6c8ba14f27276fe8b6dbe51ec030b4a19
matias-crypto <149725150+matias-crypto@users.noreply.github.com>
2024-07-09 00:03:58 -0300

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -403,16 +403,16 @@ export async function handler(chatUpdate) {
 
 global.dfail = (type, m, conn, usedPrefix) => {
     let msg = {
-        rowner: `✯ Hola, este comando solo puede ser utilizado por el *Creador* de la Bot.`,
-        owner: `✯ Hola, este comando solo puede ser utilizado por el *Creador* de la Bot y *Sub Bots*.`,
-        mods: `✯ Hola, este comando solo puede ser utilizado por los *Moderadores* de la Bot.`,
-        premium: `✯ Hola, este comando solo puede ser utilizado por Usuarios *Premium*.`,
-        group: `✯ Hola, este comando solo puede ser utilizado en *Grupos*.`,
-        private: `✯ Hola, este comando solo puede ser utilizado en mi Chat *Privado*.`,
-        admin: `✯ Hola, este comando solo puede ser utilizado por los *Administradores* del Grupo.`,
-        botAdmin: `✯ Hola, la bot debe ser *Administradora* para ejecutar este Comando.`,
-        unreg: `✯ Hola, para usar este comando debes estar *Registrado.*\n\nPara usar el bot debes registrarte primero\n\nUtiliza: */reg nombre.edad*\n\n_Ejemplo: */reg JTxs⁩.666*_\n\nNo pongas los * *`,
-        restrict: `✯ Hola, esta característica está *deshabilitada.*`  
+        rowner: `Hola, este comando solo puede ser utilizado por el *Creador* de la Bot.`,
+        owner: `Hola, este comando solo puede ser utilizado por el *Creador* de la Bot y *Sub Bots*.`,
+        mods: `Hola, este comando solo puede ser utilizado por los *Moderadores* de la Bot.`,
+        premium: `Hola, este comando solo puede ser utilizado por Usuarios *Premium*.`,
+        group: `Hola, este comando solo puede ser utilizado en *Grupos*.`,
+        private: `Hola, este comando solo puede ser utilizado en mi Chat *Privado*.`,
+        admin: `Hola, este comando solo puede ser utilizado por los *Administradores* del Grupo.`,
+        botAdmin: `Hola, la bot debe ser *Administradora* para ejecutar este Comando.`,
+        unreg: `Hola, para usar este comando debes estar *Registrado.*\n\nPara usar el bot debes registrarte primero\n\nUtiliza: */reg nombre.edad*\n\n_Ejemplo: */reg Enzo.66*_\n\n> No pongas los * *`,
+        restrict: `> Hola, esta característica está *deshabilitada.*`  
     }[type]
     if (msg) return conn.reply(m.chat, msg, m, rcanal).then(_ => m.react('✖️'))
 }
