cf1982290b27ef92f144e0f0e224811c4667e9be
matias-crypto <149725150+matias-crypto@users.noreply.github.com>
2024-07-09 16:51:47 -0300

    Actualizar verificar-registrar.js

--- plugins/verificar-registrar.js
+++ plugins/verificar-registrar.js
@@ -1,29 +1,41 @@
 import fs from 'fs';
-import { createHash } from 'crypto'
-let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
+import { createHash } from 'crypto';
+
+let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i;
 let handler = async function (m, { conn, text, usedPrefix, command }) {
-  let user = global.db.data.users[m.sender]
+  let user = global.db.data.users[m.sender];
+  
+  const data = global;
+  let idioma = data.db.data.users[m.sender].language || 'es'; // Define un idioma por defecto si no está definido
+  let _translate;
 
-        const data = global;
-        const idioma = data.db.data.users[m.sender].language;
-        const _translate = JSON.parse(fs.readFileSync(`./idiomas/${idioma}.json`, 'utf8'));
-        const traductor = _translate.plugins._registro;
+  try {
+    _translate = JSON.parse(fs.readFileSync(`./idiomas/${idioma}.json`, 'utf8'));
+  } catch (error) {
+    throw new Error(`Error al leer el archivo de traducción: ${error.message}`);
+  }
 
-  let name2 = conn.getName(m.sender)
-  if (user.registered === true) throw `🚫 𝐄𝐑𝐑𝐎𝐑 🚫 ${traductor.texto1}\n\n${traductor.texto2}\n\n💬 Use el siguiente comando*\n*${usedPrefix}unreg* <Numero de serie>`
-  if (!Reg.test(text)) throw `🚫 𝐄𝐑𝐑𝐎𝐑 🚫 *Forma incorrecta*\n\n📝 Uso correcto Del Comando: *${usedPrefix + command} nombre.edad*\n [ 💡 ] Ejemplo : *${usedPrefix + command}* ${name2}.18`
-  let [_, name, splitter, age] = text.match(Reg)
-  if (!name) throw '🚫 𝐄𝐑𝐑𝐎𝐑 🚫 El nombre no puede estar vacío*'
-  if (!age) throw '🚫 𝐄𝐑𝐑𝐎𝐑 🚫 *La edad no puede quedarse vacia*'
-  if (name.length >= 30) throw '*🚫 𝐄𝐑𝐑𝐎𝐑 🚫 El nombre es muy largo*' 
-  age = parseInt(age)
-  if (age > 100) throw '*Un dinosaurio quiere jugar con el bot?*'
-  if (age < 5) throw '*Eres menor, no  puedes registrarte en este Bot*'
-  user.name = name.trim()
-  user.age = age
-  user.regTime = + new Date
-  user.registered = true
-  let sn = createHash('md5').update(m.sender).digest('hex')
+  const traductor = _translate.plugins._registro;
+
+  let name2 = conn.getName(m.sender);
+  if (user.registered === true) throw `🚫 𝐄𝐑𝐑𝐎𝐑 🚫 ${traductor.texto1}\n\n${traductor.texto2}\n\n💬 Use el siguiente comando*\n*${usedPrefix}unreg* <Numero de serie>`;
+  if (!Reg.test(text)) throw `🚫 𝐄𝐑𝐑𝐎𝐑 🚫 *Forma incorrecta*\n\n📝 Uso correcto Del Comando: *${usedPrefix + command} nombre.edad*\n [ 💡 ] Ejemplo : *${usedPrefix + command}* ${name2}.18`;
+  
+  let [_, name, splitter, age] = text.match(Reg);
+  if (!name) throw '🚫 𝐄𝐑𝐑𝐎𝐑 🚫 El nombre no puede estar vacío*';
+  if (!age) throw '🚫 𝐄𝐑𝐑𝐎𝐑 🚫 *La edad no puede quedarse vacia*';
+  if (name.length >= 30) throw '*🚫 𝐄𝐑𝐑𝐎𝐑 🚫 El nombre es muy largo*';
+  
+  age = parseInt(age);
+  if (age > 100) throw '*Un dinosaurio quiere jugar con el bot?*';
+  if (age < 5) throw '*Eres menor, no puedes registrarte en este Bot*';
+  
+  user.name = name.trim();
+  user.age = age;
+  user.regTime = +new Date();
+  user.registered = true;
+  
+  let sn = createHash('md5').update(m.sender).digest('hex');
   m.reply(`
 °°°·.¯°·._.··._.·°°°
 > *✅ 𝑹𝑬𝑮𝑰𝑺𝑻𝑹𝑶* 
@@ -34,11 +46,11 @@ let handler = async function (m, { conn, text, usedPrefix, command }) {
 > ${sn}
 > Use *${usedPrefix}menu* para ver el menu de comandos
 ┗───── • ◆ • ─────╯
-`.trim())
+`.trim());
 }
-handler.help = ['reg'].map(v => v + ' <nombre.edad>')
-handler.tags = ['REGISTRO']
 
-handler.command = ['verify', 'verificar', 'reg', 'register', 'registrar'] 
+handler.help = ['reg'].map(v => v + ' <nombre.edad>');
+handler.tags = ['REGISTRO'];
+handler.command = ['verify', 'verificar', 'reg', 'register', 'registrar'];
 
-export default handler
+export default handler;
