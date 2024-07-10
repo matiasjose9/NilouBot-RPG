9513ea9fe91c8869ec129def9fc0663bed1c6174
matias-crypto <149725150+matias-crypto@users.noreply.github.com>
2024-07-09 21:12:51 -0300

    Actualizar link-noanti.js

--- plugins/link-noanti.js
+++ plugins/link-noanti.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn }) => {
     const linkRegex = /https?:\/\/[^\s]+/gi;
 
     const groupLink = "https://chat.whatsapp.com/IICmKudSbuGBFKMPZitp0h";
-    const channelLink = https://whatsapp.com/channel/0029VajIId22phHQLslZah1n;
+    const channelLink = "https://whatsapp.com/channel/0029VajIId22phHQLslZah1n";
 
     const insults = [
         "`Hey bro, no envíes links de mierda.`",
