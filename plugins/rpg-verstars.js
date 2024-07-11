854855a0896c0fa4805fe5f5527993e47b5a7372
matias-crypto <149725150+matias-crypto@users.noreply.github.com>
2024-07-11 20:40:05 -0300

    Actualizar rpg-verstars.js

--- plugins/rpg-verstars.js
+++ plugins/rpg-verstars.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => {
   let user = global.db.data.users[m.sender]
   let stars = user.stars || 0
-  conn.reply(m.chat, `🌟 Tienes ${stars} estrellas.`, m)
+  conn.reply(m.chat, `🌟 Tienes ${stars} estrellas.`, m, rcanal)
 }
 
 handler.help = ['verstars']
