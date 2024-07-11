99f0121890b42dca5c5f69f6db6e308a5f83d4ff
matias-crypto <149725150+matias-crypto@users.noreply.github.com>
2024-07-11 13:28:44 -0300

    Actualizar jadibot-code.js

--- plugins/jadibot-code.js
+++ plugins/jadibot-code.js
@@ -141,7 +141,6 @@ handler.tags = ['JADIBOT'];
 handler.command = ['code'];
 handler.register = true;
 handler.private = true;
-handler.rowner = true;
 export default handler;
 function sleep(ms) {
 return new Promise(resolve => setTimeout(resolve, ms));
