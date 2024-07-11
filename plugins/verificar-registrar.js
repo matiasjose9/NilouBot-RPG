128f771e32eaf80f19ed424bc5545527e6e66200
matias-crypto <149725150+matias-crypto@users.noreply.github.com>
2024-07-11 13:47:37 -0300

    Actualizar verificar-registrar.js

--- plugins/verificar-registrar.js
+++ plugins/verificar-registrar.js
@@ -3,13 +3,14 @@ import { createHash } from 'crypto'
 let handler = async function (m, { conn, usedPrefix, command }) {
   let user = global.db.data.users[m.sender]
   if (user.registered === true) throw `🚫 𝐄𝐑𝐑𝐎𝐑 🚫 *Ya estás registrado*\n\n¿Quieres volver a registrarte?\n\nUsa este comando para *eliminar tu registro*\n*${usedPrefix}unreg*`
-  
+
   user.registered = true
   user.regTime = +new Date
 
   let sn = createHash('md5').update(m.sender).digest('hex')
-  
-  await m.reply(`✅ 𝑹𝑬𝑮𝑰𝑺𝑻𝑹𝑶\n\nTu número:\n\n ${m.sender}`)
+  let description = await conn.getName(m.sender) || 'Sin descripción'
+
+  await m.reply(`✅ 𝑹𝑬𝑮𝑰𝑺𝑻𝑹𝑶\n\nTu número:\n\n${m.sender}\n\nDescripción: ${description}`)
 }
 
 handler.help = ['reg']
